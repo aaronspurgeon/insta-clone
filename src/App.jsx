0e40c7edf9e7f0062da0ccafedfffe4688418449
Aaron Spurgeon <aspurgeon061@gmail.com>
2020-08-15 18:46:54 -0400

    Setting up firebase

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,73 @@
 import React from 'react';
+import {
+  FirebaseAuthProvider,
+  IfFirebaseAuthed,
+  IfFirebaseUnAuthed
+} from "@react-firebase/auth";
+import { State } from "react-powerplug";
+import firebase from 'firebase'
+import { config } from './config'
 import './App.css';
+
+const IDontCareAboutFirebaseAuth = () => {
+  return <div>This part won't react to firebase auth changes</div>;
+};
 
 function App() {
   return (
     <div className="App">
-      <h1>poop</h1>
+      <IDontCareAboutFirebaseAuth />
+      <FirebaseAuthProvider {...config} firebase={firebase}>
+        <State initial={{ isLoading: false }}>
+          {({ state, setState }) => (
+            <React.Fragment>
+              <div>isLoading : {JSON.stringify(state.isLoading)}</div>
+              <IfFirebaseAuthed>
+                <div>
+                  <h2>You're signed in 🎉 </h2>
+                  <button
+                    onClick={async () => {
+                      setState({ isLoading: true });
+                      await firebase
+                        .app()
+                        .auth()
+                        .signOut();
+                      setState({ isLoading: false });
+                    }}
+                  >
+                    Sign out
+                  </button>
+                </div>
+              </IfFirebaseAuthed>
+              <IfFirebaseUnAuthed>
+                <div>
+                  <h2>You're not signed in </h2>
+                  <button
+                    onClick={async () => {
+                      setState({ isLoading: true });
+                      await firebase
+                        .app()
+                        .auth()
+                        .signInAnonymously();
+                      setState({ isLoading: false });
+                    }}
+                  >
+                    Sign in anonymously
+                  </button>
+                  <button
+                    onClick={() => {
+                      const googleAuthProvider = new firebase.auth.GoogleAuthProvider();
+                      firebase.auth().signInWithPopup(googleAuthProvider);
+                    }}
+                  >
+                    Sign in with Google
+                  </button>
+                </div>
+              </IfFirebaseUnAuthed>
+            </React.Fragment>
+          )}
+        </State>
+      </FirebaseAuthProvider>
     </div>
   );
 }
---
+++ src/config.js
@@ -0,0 +1,9 @@
+export const config = {
+  apiKey: "AIzaSyAVjn2dYaA-hUXLadPQPLDSqCrFXK8HEOc",
+  projectId: "insta-clone-1429d",
+  databaseURL: "https://insta-clone-1429d.firebaseio.com",
+  authDomain: "insta-clone-1429d.firebaseapp.com",
+  // OPTIONAL
+  storageBucket: "insta-clone-1429d.appspot.com",
+  messagingSenderId: "815215463627",
+};
