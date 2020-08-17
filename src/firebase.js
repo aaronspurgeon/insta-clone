1aa3004770d9f1e02ea85a99375772d7aa67e846
Aaron Spurgeon <aspurgeon061@gmail.com>
2020-08-17 15:42:25 -0400

    pausing may go a different route

--- src/firebase.js
+++ src/firebase.js
@@ -1,14 +1,6 @@
 import firebase from "firebase";
 
-const firebaseApp = firebase.initializeApp({
-  apiKey: "AIzaSyAVjn2dYaA-hUXLadPQPLDSqCrFXK8HEOc",
-  projectId: "insta-clone-1429d",
-  databaseURL: "https://insta-clone-1429d.firebaseio.com",
-  authDomain: "insta-clone-1429d.firebaseapp.com",
-  // OPTIONAL
-  storageBucket: "insta-clone-1429d.appspot.com",
-  messagingSenderId: "815215463627",
-});
+const firebaseApp = firebase.initializeApp({});
 
 const db = firebase.firestore();
 
